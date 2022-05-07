cc63be72fb9974e71b2ef2ea7d88ab0daa69de8d
epicpenguino <epicpenguinogames@gmail.com>
2022-05-07 15:33:10 -0400

    wrote documentation and refactored code

--- wordle.py
+++ wordle.py
@@ -1,6 +1,9 @@
 import random
 
-
+# Opens a dictionary text-file and makes a list of every word of a user-inputted length and then returns a random word from that list
+# dictionary -> type: string
+# length -> type: int
+# word_list -> type: list
 def word_picker(dictionary,length,word_list):
     with open(dictionary,"r") as words:
         for word in words:
@@ -9,11 +12,17 @@ def word_picker(dictionary,length,word_list):
             if len(word) == length:
                 word_list.append(word)
     if len(word_list) == 0:
-        print("No possible words that length")
-        quit()
+        print("No possible words that length, please input different length.")
+        length = int(input("What word length do you want? "))
+        word_picker(dictionary,length,word_list)
         
     return(random.choice(word_list))
 
+# Passes every word in word_list through the hints provided and returns a list of the possible answers
+# word_list -> type: list
+# wrong -> type: list
+# misplaced -> type: list
+# correct -> type: list
 def easy_mode(word_list,wrong,misplaced,correct):
     possible_list = []
     for possible in word_list:
@@ -47,6 +56,9 @@ def easy_mode(word_list,wrong,misplaced,correct):
         possible_list.append(possible)
     return(possible_list)
 
+# Run the guess through the list of possible answers provided by easy_mode function and returns a boolean of whether it is or is not
+# guess -> type: string
+# possible_list -> type: list
 def hard_mode(guess,possible_list):
     return guess not in possible_list
 
@@ -59,23 +71,22 @@ def main():
     easy_enabled = input("Would you like to play with hints mode? y or n: ") == "y"
     hard_enabled = input("Would you like to play with hard mode? y or n: ") == "y"
     
-    word_list = []
-    wrong = []
-    misplaced = [[] for i in range(length)]
-    correct = ["_" for i in range(length)]
-    win=False
+    # word_list sets up a list to put every word inside of. wrong, misplaced, and correct are the "hint" lists. win is the bool that determines whether to continue the game
+    word_list, wrong, misplaced, correct, win = [], [], [[] for i in range(length)], ["_" for i in range(length)], False
     
     wordle = word_picker(dictionary,length,word_list)
-    while win == False:
+    while not win:
         
         possible_list = easy_mode(word_list,wrong,misplaced,correct)
         if easy_enabled:
             print(possible_list)
         
-        guess = input("\nEnter your guess ")
+        guess = input("\nEnter your guess: ")
         
         if hard_enabled and hard_mode(guess,possible_list):
             print("Guess does not match hints")
+            if guess not in word_list:
+                print("Invalid guess")
             continue
         if guess not in word_list:
             print("Invalid guess")
@@ -95,9 +106,9 @@ def main():
                     wrong.append(letter)
             guess = (guess[0:spot] + "_" + guess[spot+1:])
         wrong.sort()
-        print(wrong)
-        print(misplaced)
-        print(correct)
+        print(f"Wrong letters: {wrong}")
+        print(f"Misplaced letters: {misplaced}")
+        print(f"Correctly positioned letters: {correct}")
         if correct == list(wordle):
             win=True
             print("You win")
